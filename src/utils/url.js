3f9f7f478746739e2a47f2a25d5e0e21e4a4b333
madegwa-o <madegwaoscar317@gmail.com>
2025-05-09 22:18:43 +0300

    dksdof

--- src/utils/url.js
+++
@@ -1,9 +0,0 @@
-const baseUrl = import.meta.env.VITE_BACKEND_URL || 'http://localhost:8080'; // Default fallback
-
-export default function getBaseUrl() {
-    if (!baseUrl) {
-        console.error('Backend URL is not defined.');
-        return '';
-    }
-    return baseUrl;
-}
