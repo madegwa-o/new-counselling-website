f3d04f132a53f0287011bd18605bb6c61271b4a6
madegwa-o <madegwaoscar317@gmail.com>
2025-05-09 14:37:28 +0300

    home page rebuild

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import styles from './Login.module.css';
+import styles from './login.module.css';
 
 interface FormData {
     username: string;
